3021afd12663f680dd2bdd9fdd2bf85522e69626
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2024-06-08 12:52:13 +0300

    Add files via upload

---
+++ LAB8.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] < arr[j + 1]) {
+                // Зміна місцями, якщо поточний елемент менший за наступний
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = { 5, 2, 9, 1, 7 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Виклик функції сортування
+    bubbleSort(arr, n);
+
+    // Вивід відсортованого масиву у зменшеному порядку
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
